e9a8baec70ee12bda9a54e99e619661dcca21960
brew-install-buzzwords <brewinstallbuzzwords@gmail.com>
2022-01-04 20:21:22 -0500

    remove unused code

--- lib/word_list.ex
+++ lib/word_list.ex
@@ -1,7 +1,5 @@
 defmodule WordList do
   def getStream!() do
-    # :code.priv_dir(:word_list)
-    # |> Path.join("words.txt")
     Application.app_dir(:word_list, "/priv/words.txt")
     |> File.stream!()
     |> Stream.map(fn x -> String.trim(x) end)
